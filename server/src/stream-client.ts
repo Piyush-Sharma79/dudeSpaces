6cddadcd087409a57b6396a39aa523588e4144e5
Piyush-Sharma79 <g2001idseptember@gmail.com>
2025-07-07 21:33:42 +0530

    removed secrets

--- server/src/stream-client.ts
+++ server/src/stream-client.ts
@@ -1,6 +1,13 @@
 import {StreamClient} from "@stream-io/node-sdk"
 import dotenv from "dotenv"
+
 dotenv.config()
-const API_KEY="hyfhuskn2cs3";
-const API_SECRET="um98pvqvsmg6nzcqutxtm49c4a2yhyh5ysbjfvdgm4m47saxaff4avdu4jryct23"
+
+const API_KEY = process.env.STREAM_API_KEY;
+const API_SECRET = process.env.STREAM_API_SECRET;
+
+if (!API_KEY || !API_SECRET) {
+    throw new Error("STREAM_API_KEY and STREAM_API_SECRET must be set in environment variables");
+}
+
 export const client = new StreamClient(API_KEY, API_SECRET);
