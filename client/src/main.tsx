bbcfc4d92c4573527322c6beab7d469f9bf308de
Piyush-Sharma79 <g2001idseptember@gmail.com>
2025-07-09 17:27:25 +0530

    final

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,13 +4,11 @@ import App from "./App.tsx";
 import { UserProvider } from "./user-context.tsx";
 import './fonts.css';
 import './App.css';
-import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <UserProvider>
       <App />
-      <Analytics />
     </UserProvider>
   </React.StrictMode>
 );
